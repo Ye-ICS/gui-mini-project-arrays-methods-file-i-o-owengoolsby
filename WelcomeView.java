a0c770af7c5995c3ebae019ebb8758f0f076d961
owengoolsby <owenwgoolsby@gmail.com>
2025-12-15 12:46:07 -0500

    Creating the Welcome View and SimonGame

---
+++ WelcomeView.java
@@ -0,0 +1,22 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class WelcomeView extends VBox{
+    WelcomeView() {
+        Label label = new Label("Welcome");
+        Button startBtn = new Button("Press to start");
+        getChildren().addAll(label, startBtn);
+        startBtn.setOnAction(null);
+    }
+
+    void openSimonGame() {
+        Scene currentScene = getScene();    
+        SimonGame newRoot = new SimonGame();  
+        currentScene.setRoot(newRoot);
+        currentScene.getWindow().sizeToScene(); 
+    }
+
+}
